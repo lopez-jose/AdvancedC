7893ea5737f6ff6a2c15fcf93e37bb7017dfbd80
Jose Lopez <joselopezbusiness@gmail.com>
2020-07-04 22:04:36 -0700

    Initial Commit

---
+++ Chapter 2: Review of Standard Pointer and Array Operations/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Chapter 2: Review of Standard Pointer and Array Operations
+//
+//  Created by Jose on 7/4/20.
+//  Copyright © 2020 Jose Lopez. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
